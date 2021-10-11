e0660ae9a2c247ab40093bd0e93a4dba26e791aa
jpwakugawa <jokers2s2@outlook.com>
2021-10-11 18:51:29 -0400

    NEW: buscaALuno()

--- main.cpp
+++ main.cpp
@@ -1,24 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define NOME 50
+FILE *lista;
+int cadastrados = 0;
 
 struct Aluno
 {
-    char nome[100];
+    char nome[NOME];
     int RA;
-    int P1;
-    int P2;
-    int T;
-    int PO;
+    double P1;
+    double P2;
+    double T;
+    double PO;
 };
 
-void cadastrarAluno(Aluno alunos[], int &n)
+void cadastrarAluno()
 {
-    scanf("%[^\n]", alunos[n].nome);
-    scanf("%d", &alunos[n].RA);
-    scanf("%d", &alunos[n].P1);
-    scanf("%d", &alunos[n].P2);
-    scanf("%d", &alunos[n].T);
-    scanf("%d", &alunos[n].PO);
-    n++;
+    Aluno aluno;
+    lista = fopen("lista.txt", "a");
+
+    if(lista == NULL)
+        printf("Erro na abertura do arquivo");
+    else
+    {
+        printf("Nome: "); scanf("%s", aluno.nome);
+        while((getchar()) != '\n');
+        printf("RA: "); scanf("%d", &aluno.RA);
+        while((getchar()) != '\n');
+        printf("P1: "); scanf("%lf", &aluno.P1);
+        while((getchar()) != '\n');
+        printf("P2: "); scanf("%lf", &aluno.P2);
+        while((getchar()) != '\n');
+        printf("T: "); scanf("%lf", &aluno.T);
+        while((getchar()) != '\n');
+        printf("PO: "); scanf("%lf", &aluno.PO);
+        while((getchar()) != '\n');
+
+        fprintf(lista, "%s %d %lf %lf %lf %lf\n", aluno.nome, aluno.RA, aluno.P1, aluno.P2, aluno.T, aluno.PO);
+    }
+
+    fclose(lista);
+}
+
+void buscarAluno()
+{
+    char user[NOME];
+    printf("Digite um nome: ");
+    scanf("%s", user);
+    
+    for(int i=0; i<cadastrados; i++)
+    {
+        Aluno atual;
+        fscanf(lista, "%s", atual.nome);
+        printf("%s", atual.nome);
+
+        char *ret = strstr(atual.nome, user);
+        if(ret)
+            printf("Encontrei!\n");
+        else
+            printf("Nops :(\n");
+    }
 }
 
 int main()
@@ -28,17 +70,25 @@ int main()
         printf("--- MENU ---\n");
 
         int option;
-        printf("1. Cadastrar novo aluno\n");
-        printf("2. Buscar aluno\n");
-        printf("3. Ler arquivo com listagem de alunos\n");
-        printf("4. Gerar arquivo com dados dos alunos aprovados\n");
-        printf("5. Gerar arquivo com dados dos alunos reprovados\n");
-        printf("6. Fechar programa\n");
+        printf("1. Cadastrar novo aluno.\n");
+        printf("2. Buscar aluno.\n");
+        printf("3. Ler arquivo com listagem de alunos.\n");
+        printf("4. Gerar arquivo com dados dos alunos aprovados.\n");
+        printf("5. Gerar arquivo com dados dos alunos reprovados.\n");
+        printf("6. Fechar programa.\n");
         printf("Option: ");
         scanf("%d", &option);
 
         if(option == 6)
             break;
-        
+        else if(option == 1)
+        {
+            cadastrarAluno();
+            cadastrados++;
+        }
+        else if(option == 2)
+        {
+            buscarAluno();
+        }
     }
 }
